bcb4190a0f393bbace1c2a88fd28fdfc4043f1dd
RonG <racg550@gmail.com>
2017-12-12 00:12:26 -0600

    Functional UI and API

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+
+var dom = [];
+var na = [];
+var na2 =[];
+var timer = 1000;
+var compName = "data: [";
+const https = require('https');
+const custID = '009637816073108880163:nfsysoqnztc';
+const key = 'AIzaSyBXGkVilmqEN0KSDAaQy1BlVVIA8r4nS6w';
+var finalObject = {};
+
+/*
+//API
+//app.get('/api', function(req, res){
+ 
+  if (err){
+    console.log(err);
+  }
+  	
+  	
+	//res.json(finalObject);
+	//res.render("Hello world");
+	console.log("got request");
+	res.end;
+}); */
+
