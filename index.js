05e9d48e1b2f90d200a58bcaa20a119614a7a6a1
Ronald Goebel <twotwoseven@ymail.com>
2017-12-13 21:29:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,3 @@ const custID = '009637816073108880163:nfsysoqnztc';
 const key = 'AIzaSyBXGkVilmqEN0KSDAaQy1BlVVIA8r4nS6w';
 var finalObject = {};
 
-/*
-//API
-//app.get('/api', function(req, res){
- 
-  if (err){
-    console.log(err);
-  }
-  	
-  	
-	//res.json(finalObject);
-	//res.render("Hello world");
-	console.log("got request");
-	res.end;
-}); */
-
